7a5f6a5636d2d8a5143e417086b581bae4e1b35a
jahnen <jahnen@in.tum.de>
2016-02-04 23:57:58 +0100

    ...

--- libaums/src/main/java/com/github/mjdev/libaums/server/http/UsbFileHttpServer.java
+++ libaums/src/main/java/com/github/mjdev/libaums/server/http/UsbFileHttpServer.java
@@ -3,7 +3,11 @@ package com.github.mjdev.libaums.server.http;
 import fi.iki.elonen.NanoHTTPD;
 
 /**
- * Created by mep on 04/02/16.
+ * This class allows to start a HTTP web server which can serve {@link com.github.mjdev.libaums.fs.UsbFile}s
+ * to another app.
+ *
+ * For instance it can make an image available to the Web Browser without copying it to the internal
+ * storage, or a video file to a video file as a HTTP stream.
  */
 public class UsbFileHttpServer extends NanoHTTPD {
 
