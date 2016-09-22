a3aa74c80430eda2ed5859b854f6c8c7c5d97c52
Y-E-P <mcpchip@gmail.com>
2016-09-22 16:46:14 +0300

    Added unversioned files

---
+++ libaums/src/main/java/com/github/mjdev/libaums/partition/FatType.java
@@ -0,0 +1,10 @@
+package com.github.mjdev.libaums.partition;
+
+/**
+ * Created by poudanen on 22.09.16.
+ * enum for check a current FAT type
+ */
+
+public enum FatType {
+    FAT12, FAT16, FAT32
+}
