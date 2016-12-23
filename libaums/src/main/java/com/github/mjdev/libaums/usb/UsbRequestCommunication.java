bbbaadf66cf8ca1f3d80f9b23986ccff912bc146
Magnus Jahnen <jahnen@in.tum.de>
2016-12-22 16:25:14 -0800

    UsbReqeust position workaround copy buffer

--- libaums/src/main/java/com/github/mjdev/libaums/usb/UsbRequestCommunication.java
+++ libaums/src/main/java/com/github/mjdev/libaums/usb/UsbRequestCommunication.java
@@ -16,6 +16,7 @@ class UsbRequestCommunication implements UsbCommunication {
     private UsbDeviceConnection deviceConnection;
     private UsbRequest outRequest;
     private UsbRequest inRequest;
+    private ByteBuffer workaroundBuffer = ByteBuffer.allocate(1024 * 32 * 4);
 
     UsbRequestCommunication(UsbDeviceConnection deviceConnection, UsbEndpoint outEndpoint, UsbEndpoint inEndpoint) {
         this.deviceConnection = deviceConnection;
@@ -31,18 +32,20 @@ class UsbRequestCommunication implements UsbCommunication {
     @Override
     public synchronized int bulkOutTransfer(ByteBuffer src) throws IOException {
         int length = src.remaining();
+        int oldPosition = src.position();
 
         // workaround: UsbRequest.queue always reads at position 0 :/
-        ByteBuffer tmp = ByteBuffer.wrap(src.array(), src.position(), length);
+        workaroundBuffer.clear();
+        workaroundBuffer.put(src);
 
-        if (!outRequest.queue(tmp, length)) {
+        if (!outRequest.queue(workaroundBuffer, length)) {
             throw new IOException("Error queueing request.");
         }
 
         UsbRequest request = deviceConnection.requestWait();
         if (request == outRequest) {
-            src.position(src.position() + tmp.position());
-            return tmp.position();
+            src.position(oldPosition + workaroundBuffer.position());
+            return workaroundBuffer.position();
         }
 
         throw new IOException("requestWait failed! Request: " + request);
@@ -53,15 +56,18 @@ class UsbRequestCommunication implements UsbCommunication {
         int length = dest.remaining();
 
         // workaround: UsbRequest.queue always writes at position 0 :/
-        ByteBuffer tmp = ByteBuffer.wrap(dest.array(), dest.position(), length);
-        if(!inRequest.queue(tmp, length)) {
+        workaroundBuffer.clear();
+        workaroundBuffer.limit(length);
+
+        if(!inRequest.queue(workaroundBuffer, length)) {
             throw new IOException("Error queueing request.");
         }
 
         UsbRequest request = deviceConnection.requestWait();
         if (request == inRequest) {
-            dest.position(dest.position() + tmp.position());
-            return tmp.position();
+            workaroundBuffer.flip();
+            dest.put(workaroundBuffer);
+            return workaroundBuffer.limit();
         }
 
         throw new IOException("requestWait failed! Request: " + request);
