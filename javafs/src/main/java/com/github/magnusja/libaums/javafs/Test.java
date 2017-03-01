9d7728bca5426c70129dcd4b83baed86a88858b5
Magnus Jahnen <jahnen@in.tum.de>
2017-02-28 23:11:14 -0800

    add javafs module

---
+++ javafs/src/main/java/com/github/magnusja/libaums/javafs/Test.java
@@ -0,0 +1,11 @@
+package com.github.magnusja.libaums.javafs;
+
+import org.jnode.driver.DeviceAPI;
+
+/**
+ * Created by magnusja on 2/28/17.
+ */
+
+public class Test {
+    private DeviceAPI api;
+}
