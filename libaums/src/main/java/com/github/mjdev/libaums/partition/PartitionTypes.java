874d0e1684a39d92497f6ea13997606206c66786
Magnus Jahnen <jahnen@in.tum.de>
2017-02-28 20:22:20 -0800

    Add general partition types

---
+++ libaums/src/main/java/com/github/mjdev/libaums/partition/PartitionTypes.java
@@ -0,0 +1,27 @@
+package com.github.mjdev.libaums.partition;
+
+/**
+ * Created by magnusja on 28/02/17.
+ */
+
+public class PartitionTypes {
+    public static final int UNKNOWN = -1;
+    public static final int FAT12 = 0;
+    public static final int FAT16 = 1;
+    public static final int FAT32 = 2;
+
+    public static final int EXFAT = 3;
+
+    public static final int EXT2 = 4;
+    public static final int EXT3 = 5;
+    public static final int EXT4 = 6;
+
+    public static final int HFS = 7;
+    public static final int HFS_PLUS = 8;
+
+    public static final int ISO9660 = 9;
+
+    public static final int NTFS = 10;
+
+    public static final int XFS = 11;
+}
