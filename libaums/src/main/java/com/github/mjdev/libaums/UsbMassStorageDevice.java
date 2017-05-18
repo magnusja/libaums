2ba37d5cca2ca70243aa1e033e683f046c07d6fd
Magnus Jahnen <jahnen@in.tum.de>
2017-05-18 23:31:13 +0200

    only init once

--- libaums/src/main/java/com/github/mjdev/libaums/UsbMassStorageDevice.java
+++ libaums/src/main/java/com/github/mjdev/libaums/UsbMassStorageDevice.java
@@ -88,6 +88,7 @@ public class UsbMassStorageDevice {
 	private BlockDeviceDriver blockDevice;
 	private PartitionTable partitionTable;
 	private List<Partition> partitions = new ArrayList<Partition>();
+	private boolean inited = false;
 
 	/**
 	 * Construct a new {@link com.github.mjdev.libaums.UsbMassStorageDevice}.
@@ -191,6 +192,8 @@ public class UsbMassStorageDevice {
 		else
 			throw new IllegalStateException("Missing permission to access usb device: " + usbDevice);
 
+		inited = true;
+
 	}
 
 	/**
@@ -260,6 +263,7 @@ public class UsbMassStorageDevice {
 			Log.e(TAG, "could not release interface!");
 		}
 		deviceConnection.close();
+		inited = false;
 	}
 
 	/**
