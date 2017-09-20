33f67b6a65be6b3677abd688d3ee57632764addb
Magnus Jahnen <jahnen@in.tum.de>
2017-09-19 21:17:25 -0700

    Add a different fat32 test configuration

--- libaums/src/test/java/com/github/mjdev/libaums/fs/fat32/FatFileTest2.java
+++ libaums/src/test/java/com/github/mjdev/libaums/fs/fat32/FatFileTest2.java
@@ -9,16 +9,12 @@ import org.xenei.junit.contract.ContractImpl;
 import org.xenei.junit.contract.ContractSuite;
 import org.xenei.junit.contract.IProducer;
 
-
-/**
- * Created by magnusja on 03/08/17.
- */
 @RunWith(ContractSuite.class)
 @ContractImpl(FatFile.class)
-public class FatFileTest {
+public class FatFileTest2 {
 
-    private IProducer producer = new Fat32FileSystemProducer("https://www.dropbox.com/s/nek7bu08prykkhv/expectedValues.json?dl=1",
-            "https://www.dropbox.com/s/3bxngiqmwitlucd/mbr_fat32.img?dl=1");
+    private IProducer producer = new Fat32FileSystemProducer("https://www.dropbox.com/s/9kdizg4lcolhzed/expectedValues_blocksize32K.json?dl=1",
+            "https://www.dropbox.com/s/npob26c563ujeyk/mbr_fat32_blocksize32k.img?dl=1");
 
     @Contract.Inject
     public IProducer<Pair<Fat32FileSystem, JsonObject>> makeFat32FileSystem() {
