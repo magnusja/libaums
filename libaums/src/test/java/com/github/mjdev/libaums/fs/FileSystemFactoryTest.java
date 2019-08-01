20c3afa0b58cc43b82b4c66aa3ec73417b49f154
Magnus <jahnen@in.tum.de>
2019-08-01 16:11:14 +0200

    fix test

--- libaums/src/test/java/com/github/mjdev/libaums/fs/FileSystemFactoryTest.java
+++ libaums/src/test/java/com/github/mjdev/libaums/fs/FileSystemFactoryTest.java
@@ -4,7 +4,6 @@ import com.github.mjdev.libaums.driver.BlockDeviceDriver;
 import com.github.mjdev.libaums.driver.ByteBlockDevice;
 import com.github.mjdev.libaums.driver.file.FileBlockDeviceDriver;
 import com.github.mjdev.libaums.fs.fat32.Fat32FileSystem;
-import com.github.mjdev.libaums.partition.Partition;
 import com.github.mjdev.libaums.partition.PartitionTable;
 import com.github.mjdev.libaums.partition.PartitionTableEntry;
 import com.github.mjdev.libaums.partition.PartitionTableFactory;
@@ -25,11 +24,11 @@ public class FileSystemFactoryTest {
     public void createFat32FileSystem() throws Exception {
         BlockDeviceDriver blockDevice = new ByteBlockDevice(new FileBlockDeviceDriver(
                 new URL("https://www.dropbox.com/s/3bxngiqmwitlucd/mbr_fat32.img?dl=1"),
-                2 * 512));
+                512, 2 * 512));
         blockDevice.init();
 
         PartitionTableEntry entry = new PartitionTableEntry(PartitionTypes.FAT32, 2 * 512, 1337);
-        FileSystem fs = FileSystemFactory.createFileSystem(entry, new Partition(blockDevice, entry));
+        FileSystem fs = FileSystemFactory.createFileSystem(entry, blockDevice);
 
         assertTrue(fs instanceof Fat32FileSystem);
     }
