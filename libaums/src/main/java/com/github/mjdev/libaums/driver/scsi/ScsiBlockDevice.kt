21351c5ed6edcb125b6fbe247b0c9707b50c14dc
Magnus <magnus@jahnen.me>
2019-11-15 10:57:15 +0100

    change obsolete email

--- libaums/src/main/java/com/github/mjdev/libaums/driver/scsi/ScsiBlockDevice.kt
+++ libaums/src/main/java/com/github/mjdev/libaums/driver/scsi/ScsiBlockDevice.kt
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2014 mjahnen <jahnen@in.tum.de>
+ * (C) Copyright 2014 mjahnen <github@mgns.tech>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
