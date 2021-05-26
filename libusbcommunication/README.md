f13c32f2bc203b1619f08e4f33c173035df56972
Stepan Belousov <stepan.belousov@tracmap.com>
2021-05-26 16:28:01 +1200

    Revert "Remove libusbcommunication"
    This reverts commit 24df5038b937e1b002f63702349d20c0111e9389.

---
+++ libusbcommunication/README.md
@@ -0,0 +1,63 @@
+libusb module
+=======
+
+This module provides an alternative method of communication low level with the usb device. Instead of using [Android USB Host API](https://developer.android.com/guide/topics/connectivity/usb/host), it is using [libusb](https://github.com/libusb/libusb). This is a user space usb library which can be used on Android as well. USB communication is then established using Linux usbfs or udev. For more information please consult the libusb documentation.
+
+libusb seems to provide more stable and reliable communication with the usb device compared to the API Android is providing. 
+
+
+### Licensing
+
+Unfortunately, libusb is licenced under LGPLv2.1 which might have some drawbacks if you are developing a closed source application.
+
+The LGPL says 
+
+```
+You may convey a Combined Work under terms of your choice that […] if you also
+do each of the following:
+  a) [full attribution]
+  b) [include a copy of the license]
+  c) [if you display any copyright notices, you must mention the licensed Library]
+  d) Do one of the following:
+    0) [provide means for the user to rebuild or re-link your application against
+       a modified version of the Library]
+    1) [use runtime linking against a copy already present on the system, and allow
+       the user to replace that copy]
+  e) [provide clear instructions how to rebuild or re-link your application in light
+     of the previous point]
+```
+
+where d(1) is not possible on Android, and d(0) only possible with serious overhead if you do not want to give people your source code. What is possible __in my opinion__ is that you offer people to provide you with a compiled binary of libusb and you include that back into your application which you sent back to the user. This is because of code signing issues. Another option would be to give poeple instructions on how to do code signing on their own and then offer an unsigned apk which contains everything except libusb.
+
+__Note__ that this is only my personal understanding and not general advice how you should do it. Please conduct your own research on that topic, as __I cannot provide any guarantee that this information is correct!__
+
+Refer to the following blog where someone claims that LPGL cannot be used in closed source applications under Android: https://xebia.com/blog/the-lgpl-on-android/
+
+### How to use
+
+#### Inclusion in your build.gradle
+
+```ruby
+implementation 'me.jahnen:libaums-libusbcommunication:0.1'
+```
+
+### Activate libusb communication
+
+Before using libaums you have to tell it that you want to use libusb instead of the standard Android USB Host API:
+
+```java
+UsbCommunicationFactory.registerCommunication(new LibusbCommunicationCreator());
+UsbCommunicationFactory.setUnderlyingUsbCommunication(UsbCommunicationFactory.UnderlyingUsbCommunication.OTHER);
+```
+
+You want to do that in a static block in your first activity or the constructor before doing anything else. See also https://github.com/magnusja/libaums/blob/develop/app/src/main/java/com/github/mjdev/libaums/usbfileman/MainActivity.java#L104-L108
+
+You need at least libaums v0.7.5 to be able to use the libusb module.
+
+### Compile yourself
+
+Download libusb and add the following line to your `local.properties`
+
+```
+libusb.dir=/Users/magnus/Documents/code/libusb-1.0.23
+```
---
+++ libusbcommunication/src/main/java/me/jahnen/libaums/libusbcommunication/LibusbCommunication.kt
@@ -0,0 +1,125 @@
+package me.jahnen.libaums.libusbcommunication
+
+import android.hardware.usb.*
+import android.util.Log
+import com.github.mjdev.libaums.ErrNo
+import com.github.mjdev.libaums.usb.PipeException
+import com.github.mjdev.libaums.usb.UsbCommunication
+import com.github.mjdev.libaums.usb.UsbCommunication.Companion.TRANSFER_TIMEOUT
+import com.github.mjdev.libaums.usb.UsbCommunicationCreator
+import java.io.IOException
+import java.nio.ByteBuffer
+
+
+class LibusbCommunication(
+        usbManager: UsbManager,
+        usbDevice: UsbDevice,
+        override val usbInterface: UsbInterface,
+        override val outEndpoint: UsbEndpoint,
+        override val inEndpoint: UsbEndpoint
+) : UsbCommunication {
+
+    // used to save heap address of libusb device handle
+    private var libUsbHandleArray = longArrayOf(0)
+    private val libUsbHandle: Long
+        get() = libUsbHandleArray[0]
+    private var deviceConnection: UsbDeviceConnection?
+
+    init {
+        System.loadLibrary("libusbcom")
+
+        deviceConnection = usbManager.openDevice(usbDevice)
+                ?: throw IOException("deviceConnection is null!")
+
+        if(!nativeInit(deviceConnection!!.fileDescriptor, libUsbHandleArray)) {
+            throw IOException("libusb init failed")
+        }
+
+        val claim = deviceConnection!!.claimInterface(usbInterface, true)
+        if (!claim) {
+            throw IOException("could not claim interface!")
+        }
+//        val ret = nativeClaimInterface(libUsbHandle, usbInterface.id)
+//        if (ret < 0) {
+//            throw IOException("libusb returned $ret in claim interface")
+//        }
+    }
+
+    private external fun nativeInit(fd: Int, handle: LongArray): Boolean
+    private external fun nativeClaimInterface(handle: Long, interfaceNumber: Int): Int
+    private external fun nativeClose(handle: Long, interfaceNumber: Int)
+    private external fun nativeReset(handle: Long): Int
+    private external fun nativeClearHalt(handle: Long, interfaceNumber: Int): Int
+    private external fun nativeBulkTransfer(handle: Long, endpointAddress: Int, data: ByteArray, offset: Int, length: Int, timeout: Int): Int
+    private external fun nativeControlTransfer(handle: Long, requestType: Int, request: Int, value: Int, index: Int, buffer: ByteArray, length: Int, timeout: Int): Int
+
+    override fun bulkOutTransfer(src: ByteBuffer): Int {
+        val transferred = nativeBulkTransfer(libUsbHandle, outEndpoint.address, src.array(), src.position(), src.remaining(), TRANSFER_TIMEOUT)
+        when {
+            transferred == LIBUSB_EPIPE -> throw PipeException()
+            transferred < 0 -> throw IOException("libusb returned $transferred in control transfer")
+        }
+        src.position(src.position() + transferred)
+        return transferred
+    }
+
+    override fun bulkInTransfer(dest: ByteBuffer): Int {
+        val transferred = nativeBulkTransfer(libUsbHandle, inEndpoint.address, dest.array(), dest.position(), dest.remaining(), TRANSFER_TIMEOUT)
+        when {
+            transferred == LIBUSB_EPIPE -> throw PipeException()
+            transferred < 0 -> throw IOException("libusb returned $transferred in control transfer")
+        }
+        dest.position(dest.position() + transferred)
+        return transferred
+    }
+
+    override fun controlTransfer(requestType: Int, request: Int, value: Int, index: Int, buffer: ByteArray, length: Int): Int {
+        val ret = nativeControlTransfer(libUsbHandle, requestType, request, value, index, buffer, length, TRANSFER_TIMEOUT)
+        if (ret < 0) {
+            throw IOException("libusb returned $ret in control transfer")
+        }
+        return ret
+    }
+
+    override fun resetDevice() {
+        if (!deviceConnection!!.releaseInterface(usbInterface)) {
+            Log.w(TAG, "Failed to release interface, errno: ${ErrNo.errno} ${ErrNo.errstr}")
+        }
+
+        val ret = nativeReset(libUsbHandle)
+        // if LIBUSB_ERROR_NOT_FOUND might need reenumeration
+        Log.d(TAG, "libusb reset returned $ret")
+
+        var counter = 3
+        while(!deviceConnection!!.claimInterface(usbInterface, true) && counter >= 0) {
+            if (counter == 0) {
+                throw IOException("Could not claim interface, errno: ${ErrNo.errno} ${ErrNo.errstr}")
+            }
+            Thread.sleep(800)
+            counter--
+        }
+    }
+
+    override fun clearFeatureHalt(endpoint: UsbEndpoint) {
+        val ret = nativeClearHalt(libUsbHandle, endpoint.address)
+        Log.d(TAG, "libusb clearFeatureHalt returned $ret")
+    }
+
+    override fun close() {
+        deviceConnection!!.releaseInterface(usbInterface)
+        nativeClose(libUsbHandle, usbInterface.id)
+        deviceConnection!!.close()
+    }
+
+    companion object {
+        private val TAG = LibusbCommunication::class.java.simpleName
+        private val LIBUSB_EPIPE = -9
+    }
+}
+
+class LibusbCommunicationCreator : UsbCommunicationCreator {
+    override fun create(usbManager: UsbManager, usbDevice: UsbDevice, usbInterface: UsbInterface, outEndpoint: UsbEndpoint, inEndpoint: UsbEndpoint): UsbCommunication? {
+        return LibusbCommunication(usbManager, usbDevice, usbInterface, outEndpoint, inEndpoint)
+    }
+
+}
