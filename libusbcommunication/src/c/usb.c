f2726813c6dd54b5a531e2f0a20fec356e1e9044
magnus <magnusja@users.noreply.github.com>
2022-02-05 12:44:14 +0100

    add explanation for LIBUSB_OPTION_WEAK_AUTHORITY

--- libusbcommunication/src/c/usb.c
+++ libusbcommunication/src/c/usb.c
@@ -11,6 +11,9 @@ Java_me_jahnen_libaums_libusbcommunication_LibusbCommunication_nativeInit(JNIEnv
     LOG_D(TAG, "init native libusb");
     int ret;
 
+// see https://github.com/magnusja/libaums/pull/335/files
+// for 1.0.25 LIBUSB_OPTION_NO_DEVICE_DISCOVERY is same as LIBUSB_OPTION_WEAK_AUTHORITY
+// essentially avoids enumerating USB devices which might cause trouble on non rooted devices
 #if defined(LIBUSB_API_VERSION) && (LIBUSB_API_VERSION >= 0x01000108)
     ret = libusb_set_option(NULL, LIBUSB_OPTION_WEAK_AUTHORITY);
     if (ret != 0) {
