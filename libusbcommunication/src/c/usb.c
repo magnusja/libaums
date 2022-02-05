5b178aefdeba3e59b9967ffae6932d16bf9cee43
Cliff Garvis <cgarvis@launchcg.com>
2022-02-04 18:54:29 -0800

    Add check for libusb api version before calling libusb_set_option

--- libusbcommunication/src/c/usb.c
+++ libusbcommunication/src/c/usb.c
@@ -9,11 +9,15 @@
 JNIEXPORT jboolean JNICALL
 Java_me_jahnen_libaums_libusbcommunication_LibusbCommunication_nativeInit(JNIEnv *env, jobject thiz, jint fd, jlongArray handle) {
     LOG_D(TAG, "init native libusb");
-    int ret = libusb_set_option(NULL, LIBUSB_OPTION_NO_DEVICE_DISCOVERY, NULL);
+    int ret;
+
+#if defined(LIBUSB_API_VERSION) && (LIBUSB_API_VERSION >= 0x01000108)
+    ret = libusb_set_option(NULL, LIBUSB_OPTION_WEAK_AUTHORITY);
     if (ret != 0) {
         LOG_E(TAG, "libusb_set_option returned %d, %s", ret, libusb_strerror(ret));
         return (jboolean) JNI_FALSE;
     }
+#endif
 
     ret = libusb_init(NULL);
     if (ret != 0) {
